8e89c44364f69c534688e233726cf871d5117bab
AdirMarom <63256057+AdirMarom@users.noreply.github.com>
2020-05-03 20:57:40 +0300

    adir_last_change

--- maze/src/IO/MyCompressorOutputStream.java
+++ maze/src/IO/MyCompressorOutputStream.java
@@ -25,6 +25,7 @@ public class MyCompressorOutputStream extends OutputStream {
      */
     public void write(byte[] bytes){
         System.out.println(Arrays.toString(bytes));
+        System.out.println(Arrays.toString(bytes).length());
         //compress parameter;
         byte[] param_array=Arrays.copyOfRange(bytes,0,30);
 
@@ -59,8 +60,14 @@ public class MyCompressorOutputStream extends OutputStream {
         byte[] arrayBinary = new byte[split.length];
         for(int i=0;i<split.length;i++){
             arrayBinary[i] = (byte)Integer.parseInt(split[i],2);
+           // temp_arr[i]=(byte)Byte.toUnsignedInt(arrayBinary[i]);
+            //arrayBinary[i]=convertToUnsigh(temp_arr[i]);
+          //  if(arrayBinary[i]<0)
+            //    arrayBinary[i]=(byte)(arrayBinary[i]+255);
+            //arrayBinary[i]= (byte) (temp_arr[i] & 0xFF);
         }
         return arrayBinary;
                 //Base64.getEncoder().encodeToString(arrayBinary);
     }
+
 }
--- maze/src/IO/MyDecompressorInputStream.java
+++ maze/src/IO/MyDecompressorInputStream.java
@@ -2,8 +2,8 @@ package IO;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Base64;
 
 public class MyDecompressorInputStream extends InputStream {
     private InputStream in;
@@ -33,7 +33,7 @@ public class MyDecompressorInputStream extends InputStream {
         byte[] b=new byte[s.length()];
         int[] temp=new int[s.length()];
         for(int i=0 ;i<s.length() ;i++){
-            temp[i]=(int)s.charAt(i);
+            temp[i]=Integer.parseInt(String.valueOf(s.charAt(i)));
             b[i]=(byte)temp[i];
         }
         return b;
@@ -44,42 +44,69 @@ public class MyDecompressorInputStream extends InputStream {
         byte[] param_array=Arrays.copyOfRange(compress_byte,0,30);
         int len_matrix=get_Dsize(param_array);
 
+
         //compress value
         byte[] array_value=Arrays.copyOfRange(compress_byte,30,compress_byte.length);
-       // String base64_str=new String(array_value);
-        // System.out.println(base64_str);
-       // byte[] Dcompress=base64_str.getBytes();
-       // System.out.println(Arrays.toString(Dcompress));
-        // Base64.getDecoder().decode(base64_str);
+        int[] Unsigh_value=convert_ByteSigh_to_UnSigh(array_value);
 
-        String value_string= Arrays.toString(array_value);
         String[] D_to_binary_compress=new String[len_matrix];
-
         StringBuilder temp_str=new StringBuilder();
-
+        int Dcomp_len=0;
         for (int j=0 ;j<array_value.length;j++){
 
-            D_to_binary_compress[j]= Integer.toBinaryString(array_value[j]);
-
+            D_to_binary_compress[j]= Integer.toBinaryString(Unsigh_value[j]);
             temp_str.delete(0,temp_str.capacity());
-            temp_str.append( D_to_binary_compress[j]);
-
-            while (temp_str.length()<8 && j*8<len_matrix){
-                temp_str.insert(0,0);
-                if(temp_str.length()==8)
-                    D_to_binary_compress[j]=temp_str.toString();
+            temp_str.append(D_to_binary_compress[j]);
+            Dcomp_len+= D_to_binary_compress[j].length();
+            while (temp_str.length()<8 && Dcomp_len<len_matrix){
+                if(j==(array_value.length-1)){
+                    if(Dcomp_len<len_matrix){
+                        temp_str.insert(0, 0);
+                        Dcomp_len++;
+                        if (temp_str.length() == 8)
+                            D_to_binary_compress[j] = temp_str.toString();
+                    }
+                }
+                else{
+                    temp_str.insert(0, 0);
+                    Dcomp_len++;
+                    if (temp_str.length() == 8)
+                        D_to_binary_compress[j] = temp_str.toString();
+                }
             }
+
         }
         String D_value_string= Arrays.toString(D_to_binary_compress);
         D_value_string=D_value_string.replace(",","");
         D_value_string=D_value_string.replace(" ","");
-        D_value_string=D_value_string.substring(0,len_matrix-1);
-
+        D_value_string=D_value_string.substring(1,len_matrix-1);
         this.Dcomp_array=str_array_to_byte(D_value_string);
-
+        byte[] res=concatenate(param_array,this.Dcomp_array);
+        this.Dcomp_array=res;
         System.out.println(Arrays.toString(Dcomp_array));
+        System.out.println(Arrays.toString(Dcomp_array).length());
 
         return 0;
     }
+
+    private int[] convert_ByteSigh_to_UnSigh(byte[] arr){
+        int[] res=new int[arr.length];
+        for(int i=0 ;i<arr.length;i++){
+            res[i]=Byte.toUnsignedInt(arr[i]);
+        }
+        return res;
+    }
+
+    public byte[] concatenate(byte[] a, byte[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+
+        byte[] c = (byte[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
+        System.arraycopy(a, 0, c, 0, aLen);
+        System.arraycopy(b, 0, c, aLen, bLen);
+
+        return c;
+    }
+
 
 }
