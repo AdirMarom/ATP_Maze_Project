b92d73474eef3209c42540644f4ce99483e99dfa
AdirMarom <63256057+AdirMarom@users.noreply.github.com>
2020-05-15 18:18:55 +0300

    adir update

---
+++ maze/src/Client/Client.java
@@ -0,0 +1,38 @@
+package Client;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+    private InetAddress host;
+    private final int port;
+    private IClientStrategy clientStrategy;
+
+    public Client(InetAddress host, int port, IClientStrategy strategy ) {
+        this.host = host;
+        this.port = port;
+        this.clientStrategy = strategy;
+
+    }
+
+
+
+    public void start(InetAddress serverIP, int port)
+    {
+        try {
+            Socket socket = new Socket(serverIP,port);
+            System.out.println("Client is connected to server!");
+            Thread.sleep(1000);
+            clientStrategy.clientStrategy(socket.getInputStream(),socket.getOutputStream());
+            socket.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void communicateWithServer() {
+        start(host,port);
+    }
+}
